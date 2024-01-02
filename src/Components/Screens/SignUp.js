90b0e986ec76310ebcd0d395fbac8c94dad0d0e2
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-01-02 18:02:37 +0000

    add validation to form entry

--- src/Components/Screens/SignUp.js
+++ src/Components/Screens/SignUp.js
@@ -1,59 +1,162 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, Form, TextInput, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, TextInput, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useForm, Controller } from "react-hook-form";
 
 function SignUp() {
   const [name, setName] = useState();
   const [phoneNumber, setPhoneNumber] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
 
-  const handleSubmit = () => {
-    //Add logic to submit to firebase
-    if (!name.trim()) {
-      alert("Please enter your name");
-      return;
-    }
-    if(!phoneNumber.trim()) {
-      alert("Please enter your phone number");
-      return;
-    }
-  }
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      fullName: "",
+      phoneNumber: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = () =>
+    console.log(
+      "Signing up with",
+      name,
+      phoneNumber,
+      email,
+      password,
+      confirmPassword
+    );
+
+  const phoneNumberValidation = {
+    required: { value: true, message: "Phone number is required" },
+    pattern: {
+      value: /((\+44(\s\(0\)\s|\s0\s|\s)?)|0)7\d{3}(\s)?\d{6}/g,
+      message: "Invalid UK phone number",
+    },
+  };
+
+  const emailValidation = {
+    required: { value: true, message: "Email is required" },
+    pattern: {
+      value:
+        /([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)*|\[((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|IPv6:((((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){6}|::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){5}|[0-9A-Fa-f]{0,4}::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){4}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):)?(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){3}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,2}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){2}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,3}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,4}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,5}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,6}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)|(?!IPv6:)[0-9A-Za-z-]*[0-9A-Za-z]:[!-Z^-~]+)])/g,
+      message: "Invalid email",
+    },
+  };
+
+  const passwordValidation = {
+    required: { value: true, message: "Password is required" },
+    pattern: {
+      value:
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+      message: "Invalid password",
+    },
+  };
+
+  const confirmPasswordValidation = {
+    required: { value: true, message: "Please re-enter the password" },
+    pattern: {
+      value:
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+      message: "Password does not match",
+    },
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Sign Up</Text>
-        <TextInput
-          placeholder="Full Name"
-          onChangeText={text => setName(text)}
-        />
-        <TextInput
-          placeholder="Phone Number"
-          onChangeText={text => setPhoneNumber(text)}
-        />
-        <TextInput
-          placeholder="Email"
-          onChangeText={text => setEmail(text)}
-        />
-        <TextInput
-          placeholder="Password"
-          onChangeText={text => setPassword(text)}
-          secureTextEntry
-        />
-        <TextInput
-          placeholder="Confirm Password"
-          onChangeText={text => setPassword(text)}
-          secureTextEntry
-        />
-        <TouchableOpacity onPress={() => handleSubmit()}>
-          <Text>Submit</Text>
-        </TouchableOpacity>
-      </View>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Full Name"
+            onBlur={onBlur}
+            // onChangeText={(onChange) => setName(onChange)}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="fullName"
+      />
+      {errors.fullName && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={phoneNumberValidation}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Phone Number"
+            onBlur={onBlur}
+            // onChangeText={(onChange) => setPhoneNumber(onChange)}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="phoneNumber"
+      />
+      {errors.phoneNumber && <Text>{errors.phoneNumber.message}</Text>}
+
+      <Controller
+        control={control}
+        rules={emailValidation}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Email"
+            onBlur={onBlur}
+            // onChangeText={(onChange) => setEmail(onChange)}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="email"
+      />
+      {errors.email && <Text>{errors.email.message}</Text>}
+
+      <Controller
+        control={control}
+        rules={passwordValidation}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Password"
+            onBlur={onBlur}
+            // onChangeText={(onChange) => setEmail(onChange)}
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry
+          />
+        )}
+        name="password"
+      />
+      {errors.password && <Text>{errors.password.message}</Text>}
+
+      <Controller
+        control={control}
+        rules={passwordValidation}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Confirm Password"
+            onBlur={onBlur}
+            onChangeText={(text) => setConfirmPassword(text)}
+            value={value}
+            secureTextEntry
+          />
+        )}
+        name="confirmPassword"
+      />
+      {errors.confirmPassword && <Text>{errors.confirmPassword.message}</Text>}
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </SafeAreaView>
   );
 }
-
 export default SignUp;
 
 const styles = StyleSheet.create({
@@ -62,6 +165,3 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
 });
-
-
-
