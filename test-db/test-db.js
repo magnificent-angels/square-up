d90297714883943e9ccfc11276cd03bc3ff0c782
Gilaine Young <gilaineyoung@gmail.com>
2024-01-02 12:33:20 +0000

    Adds sample user data

---
+++ test-db/test-db.js
@@ -0,0 +1,30 @@
+export const users = [
+    {
+        name: "Princess Carolyn",
+        userId: "38cQvyhQkZb39uOnm9StWCzMfXS2",
+        email: "princess.carolyn@hollywoo.com",
+        avatarURL: "",
+        password: 'random123?',
+        location: 'Sheffield',
+        gamesOwned: [
+            {
+                gameId: "2536",
+                gameName: "Vabanque",
+                gameImageUrl: "https://cf.geekdo-images.com/wOZ1EZRFzwG4v3oVAcgDQg__original/img/XY5qcnbpQr6E-uCk0LmSEWssu48=/0x0/filters:format(jpeg)/pic5196896.jpg"
+            },
+            {
+                gameId: "1406",
+                gameName: "Monopoly",
+                gameImageUrl: "https://cf.geekdo-images.com/9nGoBZ0MRbi6rdH47sj2Qg__original/img/bA8irydTCNlE38QSzM9EhcUIuNU=/0x0/filters:format(jpeg)/pic5786795.jpg"
+            }
+        ]
+    },
+    {
+        name: "Bojack Horseman",
+        userId: "L5F1s1VUNuMY8q1f6Wup17osniV2",
+        email: "bojack@secretariat.com",
+        avatarURL: "",
+        password: 'password',
+        gamesOwned: []
+    }
+]
