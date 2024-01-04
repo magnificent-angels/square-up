9c79c829f577f3e8ee161c1109692d6d3d8fe617
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-01-04 17:09:21 +0000

    add icons for on press function on profile

--- src/Components/Screens/Profile.js
+++ src/Components/Screens/Profile.js
@@ -2,36 +2,70 @@ import { View, StyleSheet } from "react-native";
 import { useContext } from "react";
 import { UserContext } from "../../../Context/UserContext";
 import SignOut from "./SignOut";
-import { Layout, Card, Text, Button, Avatar, Divider } from "@ui-kitten/components";
-
+import {
+  Layout,
+  Card,
+  Text,
+  Button,
+  Avatar,
+  Divider,
+} from "@ui-kitten/components";
+import { Ionicons } from "@expo/vector-icons";
 
 function Profile() {
   const { user } = useContext(UserContext);
   return (
     <>
       <View style={styles.container}>
-      <SignOut />
-          <Card style={styles.profileContainer}>
-            <Avatar size="giant" source={require('../../../assets/avatars/Avatar3.png')} style={styles.content} />
-            <Text category="h1" status="info">John Doe</Text>
-            <Divider />
-            <Text category="s1" style={styles.content}>No Shows: 0</Text>
-          </Card>
-          <Card style={styles.contentContainer}>
-            <Text category="h5" >Favourite Games</Text>
-            <Divider />
-            <Text category="s1" style={styles.content}>No Favourite Games</Text>
-          </Card>
-          <Card style={styles.contentContainer}>
-            <Text category="h5" >Owned Games</Text>
-            <Divider />
-            <Text category="s1" style={styles.content}>No Owned Games</Text>
-          </Card>
-          <Card style={styles.contentContainer}>
-            <Text category="h5" >Joined Events</Text>
-            <Divider />
-            <Text category="s1" style={styles.content}>No Events Joined</Text>
-          </Card>
+        <SignOut />
+        <Card style={styles.profileContainer}>
+          <Avatar
+            size="giant"
+            source={require("../../../assets/avatars/Avatar3.png")}
+            style={styles.content}
+          />
+          <Text category="h1" status="info">
+            John Doe
+          </Text>
+          <Divider />
+          <Text category="s1" style={styles.content}>
+            No Shows: 0
+          </Text>
+        </Card>
+        <Card style={styles.contentContainer}>
+          <Ionicons
+            name="create"
+            style={styles.editIcon}
+            size={25}
+            onPress={console.log("edit favourites")}
+          />
+          <Text category="h5">Favourite Games</Text>
+
+          <Divider />
+          <Text category="s1" style={styles.content}>
+            No Favourite Games
+          </Text>
+        </Card>
+        <Card style={styles.contentContainer}>
+          <Ionicons
+            name="add-circle-outline"
+            style={styles.addIcon}
+            size={25}
+            onPress={console.log("organise game")}
+          />
+          <Text category="h5">Owned Games</Text>
+          <Divider />
+          <Text category="s1" style={styles.content}>
+            No Owned Games
+          </Text>
+        </Card>
+        <Card style={styles.contentContainer}>
+          <Text category="h5">Joined Events</Text>
+          <Divider />
+          <Text category="s1" style={styles.content}>
+            No Events Joined
+          </Text>
+        </Card>
       </View>
     </>
   );
@@ -44,7 +78,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    width:"100%"
+    width: "100%",
   },
   profileContainer: {
     width: "90%",
@@ -68,5 +102,18 @@ const styles = StyleSheet.create({
     borderTopColor: "#2766f9",
     borderTopWidth: 4,
     boxShadow: "0px 0px 5px 5px rgba(0,0,0,0.9)",
-  }
+    position: "relative",
+  },
+  editIcon: {
+    top: 8,
+    right: -50,
+    index: 1,
+    position: "absolute",
+  },
+  addIcon: {
+    top: 10,
+    right: -65,
+    index: 1,
+    position: "absolute",
+  },
 });
