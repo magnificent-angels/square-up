4043cc431e6a2bece703419178c63aa480475da2
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-01-09 18:07:21 +0000

    start map markers integration, pull event info for one event from database

--- src/Components/Screens/Map.js
+++ src/Components/Screens/Map.js
@@ -1,23 +1,47 @@
-import React, { useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { UserContext } from "../Context/UserContext";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import { db } from "../../firebase";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { doc, getDoc } from "firebase/firestore";
 
 function Map() {
   const mapRef = useRef(null);
+  const { user, events, setEvents } = useContext(UserContext);
+
+  const { latitude, longitude } = user;
   const [region, setRegion] = useState({
-    latitude: 51.5079145,
-    longitude: -0.0899163,
+    latitude: latitude,
+    longitude: longitude,
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
   });
 
+
+  useEffect(() => {
+    const docRef = doc(db, 'events', '11EIhyDjUJWu1HOcba7R');
+    getDoc(docRef)
+      .then((result) => {
+      const data = result.data();
+      setEvents(data)
+      console.log(events.location.latitude);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
   return (
-    <MapView
+    <>
+      <MapView
+        provider={PROVIDER_GOOGLE}
       ref={mapRef}
       style={styles.map}
       initialRegion={region}
       onRegionChangeComplete={(region) => setRegion(region)}
-    />
+      />
+    </>
   );
 }
 
@@ -26,5 +50,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 });
+
+
 
 export default Map;
