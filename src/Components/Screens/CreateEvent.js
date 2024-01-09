a48a7affdec38e3f6894cc288b8f8e15f347bc33
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-01-09 01:16:16 +0000

    Clean up styling and use UI framework across all pages, change gamescreen to render with a modal, add more loading spinners for user experience

--- src/Components/Screens/CreateEvent.js
+++ src/Components/Screens/CreateEvent.js
@@ -1,116 +1,113 @@
-import { Text, TextInput, View, StyleSheet, Button } from 'react-native'
-import { auth } from '../../firebase'
-import { doc, getDoc, GeoPoint, addDoc, collection, Timestamp } from 'firebase/firestore'
-import { db } from '../../firebase'
-import { useEffect, useState } from 'react'
-import DateTimePicker from 'react-native-ui-datepicker';
-import dayjs from 'dayjs';
-import { getLatLong } from '../../utils/postcodeLookup'
+import { Layout, Text, Input, Button, Spinner, RangeCalendar } from '@ui-kitten/components';
+import { useState, useEffect } from 'react';
+import { getLatLong } from '../../utils/postcodeLookup';
+import { auth, db } from '../../firebase';
+import { doc, getDoc, GeoPoint, addDoc, collection } from 'firebase/firestore';
+import { StyleSheet } from 'react-native';
 
-function CreateEvent({game, setEventBeingCreated, setEventCreated}) {
+function CreateEvent({ game, setEventBeingCreated, setEventCreated }) {
+  const [userData, setUserData] = useState(null);
+  const [eventName, setEventName] = useState('');
+  const [dateRange, setDateRange] = useState({});
+  const [postcode, setPostcode] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [userData, setUserData] = useState(null)
-  const [eventName, setEventName] = useState('')
-  const [eventDate, setEventDate] = useState(dayjs())
-  const [deadline, setDeadline] = useState(dayjs())
-  const [postcode, setPostcode] = useState('')
-  const [isLoading, setIsLoading] = useState(true)
-
-  
-  const user = auth.currentUser
-  const { name, description, minPlayers, maxPlayers, playingTime, imageUrl } = game
-
-  const handleDates = (dateString) => {
-    const date = new Date(dateString)
-    const milliseconds = date.getTime()
-    return milliseconds
-  }
+  const user = auth.currentUser;
+  const { name, minPlayers, maxPlayers, playingTime, imageUrl } = game;
 
   const onSubmit = () => {
-    getLatLong(postcode)
-    .then(({latitude, longitude}) => {
-      const dateTime = handleDates(eventDate)
-      const eventDeadline = handleDates(deadline)
-      return [dateTime, eventDeadline, latitude, longitude]
-    })
-    .then(([dateTime, eventDeadline, latitude, longitude]) => {
-      addDoc(collection(db, "events"), {
-        location: new GeoPoint(latitude, longitude),
-        eventName,
-        dateTime,
-        eventDeadline,
-        organiserUsername: userData.username,
-        organiserUid: user.uid,
-        gameName: name,
-        minPlayers: +minPlayers,
-        maxPlayers: +maxPlayers,
-        playingTime: +playingTime,
-        imageUrl,
-      })
-    })
-    .then(() => {
-      setEventName('')
-      setEventDate(dayjs())
-      setDeadline(dayjs())
-      setPostcode('')
-      setEventCreated(true)
-      setEventBeingCreated(false)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }
+    if (dateRange.startDate && dateRange.endDate) {
+      getLatLong(postcode)
+        .then(({ latitude, longitude }) => {
+          const dateTime = dateRange.startDate.getTime();
+          const eventDeadline = dateRange.endDate.getTime();
+          return [dateTime, eventDeadline, latitude, longitude];
+        })
+        .then(([dateTime, eventDeadline, latitude, longitude]) => {
+          return addDoc(collection(db, 'events'), {
+            location: new GeoPoint(latitude, longitude),
+            eventName,
+            dateTime,
+            eventDeadline,
+            organiserUsername: userData.username,
+            organiserUid: user.uid,
+            gameName: name,
+            minPlayers: +minPlayers,
+            maxPlayers: +maxPlayers,
+            playingTime: +playingTime,
+            imageUrl,
+          });
+        })
+        .then(() => {
+          setEventName('');
+          setDateRange({});
+          setPostcode('');
+          setEventCreated(true);
+          setEventBeingCreated(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
 
   useEffect(() => {
-    const docRef = doc(db, "users", user.uid)
-    getDoc(docRef)
-    .then((result) => {
-      setUserData(result.data())
-      setIsLoading(false)
-    })
-  }, [])
+    const docRef = doc(db, 'users', user.uid);
+    getDoc(docRef).then((result) => {
+      setUserData(result.data());
+      setIsLoading(false);
+    });
+  }, []);
 
-
-  if (isLoading) return <Text>Loading...</Text>
+  if (isLoading) return (
+    <Layout style={styles.loadingContainer}>
+      <Spinner size='giant' />
+    </Layout>
+  );
 
   return (
-  <View>
-    <Text>Organise an event, {userData.username}!</Text>
-    <Text style={styles.label}>Name of event: </Text>
-    <TextInput
-      style={styles.input}
-      onChangeText={(input) => setEventName(input)}
-      value={eventName}
-    />
-    <Text>Select the date and time for your event: </Text>
-    <DateTimePicker 
-      value={eventDate}
-      onValueChange={(date) => setEventDate(handleDates(date))}
-    />    
-    <Text>What is the deadline for signing up?</Text>
-    <DateTimePicker 
-      value={deadline}
-      onValueChange={(date) => setDeadline(date)}
-    />
-    <Text>Enter a postcode for the event: </Text>
-    <TextInput 
-      style={styles.input}
-      onChangeText={(input) => setPostcode(input)}
-      value={postcode}
-      placeholder="eg M1 7ED"
-    />
-    <Button title="Submit" onPress={onSubmit}></Button>
-  </View>
-  );  
+    <Layout style={styles.container}>
+      <Text category="h5">Organise an event {userData.username}!</Text>
+      <Input
+        placeholder="Enter event name"
+        value={eventName}
+        onChangeText={setEventName}
+        style={styles.input}
+        label={"Enter a name for the event:"}
+      />
+      <Text>Select the date range for your event:</Text>
+      <RangeCalendar
+        range={dateRange}
+        onSelect={setDateRange}
+      />
+      <Input
+        placeholder="eg M1 7ED"
+        value={postcode}
+        onChangeText={setPostcode}
+        style={styles.input}
+        label={"Enter a postcode for the event:"}
+      />
+      <Button onPress={onSubmit} style={styles.button}>
+        Submit
+      </Button>
+    </Layout>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
   label: {
-    fontSize: 16
+    marginVertical: 8,
   },
   input: {
-    marginBottom: 10
-  }
-})
+    marginBottom: 10,
+  },
+  button: {
+    marginVertical: 10,
+  },
+});
 
-export default CreateEvent
+export default CreateEvent;
