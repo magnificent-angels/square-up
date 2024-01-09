249fc4be7b056c7b542a996256f2b9a2bbbfa7f0
rosie562 <rosieclairegarrett@gmail.com>
2024-01-09 11:44:59 +0000

    adds state to dep array

--- src/Components/Screens/Profile.js
+++ src/Components/Screens/Profile.js
@@ -24,7 +24,7 @@ const Profile = () => {
       setOwned(userData.owned);
       setEvents(userData.events);
     });
-  }, []);
+  }, [wishlist, owned]);
   
   const renderGameItem = ({ item }) => (
     <Card style={styles.gameItemContainer} disabled >
