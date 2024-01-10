c31e3424b9ef0fedc93be8ac8cc739740132cfa0
Jonathan Brierley <jonnywb@users.noreply.github.com>
2024-01-10 23:50:08 +0000

    merged game screen tweaks

--- src/Components/Screens/Profile.js
+++ src/Components/Screens/Profile.js
@@ -1,21 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
-import {
-  StyleSheet,
-  FlatList,
-  ScrollView,
-  StatusBar,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import {
-  Layout,
-  Text,
-  Avatar,
-  Divider,
-  Card,
-  Spinner,
-  Button,
-} from "@ui-kitten/components";
+import { StyleSheet, FlatList, ScrollView, StatusBar, TouchableOpacity, View } from "react-native";
+import { Layout, Text, Avatar, Divider, Card, Spinner, Button } from "@ui-kitten/components";
 import { UserContext } from "../Context/UserContext";
 import SignOut from "./SignOut";
 import { db } from "../../firebase";
@@ -24,8 +9,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 
 const Profile = () => {
-  const { user, wishlist, setWishlist, owned, setOwned, events, setEvents } =
-    useContext(UserContext);
+  const { user, wishlist, setWishlist, owned, setOwned, events, setEvents } = useContext(UserContext);
   const { photoURL, displayName, uid } = user;
   const [loading, setLoading] = useState(true);
 
@@ -91,11 +75,7 @@ const Profile = () => {
           <SignOut />
 
           <Card style={styles.profileBox} disabled status="primary">
-            <Avatar
-              size="giant"
-              source={{ uri: photoURL }}
-              style={styles.avatar}
-            />
+            <Avatar size="giant" source={{ uri: photoURL }} style={styles.avatar} />
             <Text category="h1" style={styles.username}>
               {displayName}
             </Text>
